5da7004c7b072e3b587a60348b9ee9eacfe17375
Michael Uemura <michaeluemura@michaels-mini>
2018-03-24 08:56:33 -0700

    Add map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Michael Uemura on 3/24/18.
+//  Copyright © 2018 Michael Uemura. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
