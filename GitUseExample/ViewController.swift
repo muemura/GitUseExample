cfcb2c9932da39284621c60cda5232e183792204
Michael Uemura <michaeluemura@michaels-mini>
2018-03-24 09:12:10 -0700

    Discard button to log

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -20,9 +20,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
-    }
 
 }
 
